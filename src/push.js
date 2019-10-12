e5e38b6ce2e551632aebd612f0e0a87c99df951d
D3Portillo <d3portillo@gmail.com>
2019-10-12 12:11:08 -0600

    merges_data[bot] :zap:

--- src/push.js
+++ src/push.js
@@ -1,11 +1,18 @@
 const token = process.env.TOKEN || false
 const git = require("simple-git")
 const url = `https://D3Portillo:${token}@github.com/D3Portillo/self-taught-devs.git`
-if (token) {
+const push = () => {
   git()
-    .removeRemote("origin")
     .checkout("master")
     .add(".")
     .commit("merges_data[bot] :zap:")
     .push(url, "master")
+}
+if (token) {
+  try {
+    git().removeRemote("origin")
+    push()
+  } catch (_) {
+    push()
+  }
 } else console.log("Token not provided")
