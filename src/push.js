4ce9522b667c754c7c404a62f839935ba6050d4c
Denny <d3portillo@gmail.com>
2019-10-12 11:47:58 -0600

    merges_data[bot] :zap:

--- src/push.js
+++ src/push.js
@@ -3,9 +3,7 @@ const git = require("simple-git")
 const remote = `https://D3Portillo:${token}@self-taught-devs`
 if (token) {
   git()
-    .checkout("master")
-    .stash("apply")
     .add(".")
-    .commit("Mergin changes [bot] :zap:")
+    .commit("merges_data[bot] :zap:")
     .push(remote)
 } else console.log("Token not provided")
