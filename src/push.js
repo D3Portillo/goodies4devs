b379dc6d258276ff52301f8ff72d8707b3a5d495
D3Portillo <d3portillo@gmail.com>
2019-10-12 11:51:35 -0600

    merges_data[bot] :zap:

--- src/push.js
+++ src/push.js
@@ -3,6 +3,8 @@ const git = require("simple-git")
 const remote = `https://D3Portillo:${token}@self-taught-devs`
 if (token) {
   git()
+    .addConfig("user.name", "D3Portillo")
+    .addConfig("user.email", "d3portillo@gmail.com")
     .checkout("master")
     .add(".")
     .commit("merges_data[bot] :zap:")
