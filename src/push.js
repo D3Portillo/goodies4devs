6972e15ede27b7b45adc084c7dfd785f3688caea
Denny Portillo <d3portillo@gmail.com>
2020-03-17 20:55:09 -0600

    well...

--- src/push.js
+++ src/push.js
@@ -1,8 +1,9 @@
-const token = process.env.TOKEN || false
 const git = require("simple-git")
-const url = `https://D3Portillo:${token}@github.com/D3Portillo/self-taught-devs.git`
-
-if (token) {
+const TOKEN = process.env.TOKEN || false
+const USER = "d3portillo@gmail.com"
+const REPO = "github.com/D3Portillo/self-taught-devs.git"
+const url = `https://${USER}:${TOKEN}@${REPO}`
+if (TOKEN) {
   git()
   .checkout("master")
   .add(".")
